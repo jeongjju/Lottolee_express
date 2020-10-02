911d29abcf60b28635f70013f140f27ef0cc9190
jj <houe007@gmail.com>
2020-10-03 07:52:19 +0900

    로또 크롤링 오류로 인하여 수정

--- crawling/index.js
+++ crawling/index.js
@@ -19,19 +19,41 @@ getHtml()
     .then(html => {
         let ulList = [];
         const $ = cheerio.load(html.data);
-        const $bodyList = $("table.tbl_data tbody").children("tr");
-        $bodyList.each(function (i, elem) {
-            const td = $(this).find('td');
-            ulList[i]={
-                grade:anyToUtf8(td.eq(0).text()),
-                gradeTotSellamnt:anyToUtf8(td.eq(1).text()),
-                winamnt:anyToUtf8(td.eq(3).text())
-            }
-            // log('순위',anyToUtf8(td.eq(0).text()));
-            // log('등위별 총 당첨금액',anyToUtf8(td.eq(1).text()));
-            // log('당첨금액',anyToUtf8(td.eq(3).text()));
-        });
-        console.log(ulList);
+        const $numwinP = $("div.num").children("p");
+        console.log($numwinP.find('span').eq(0).text());
+        console.log($numwinP.find('span').eq(1).text());
+        console.log($numwinP.find('span').eq(2).text());
+        console.log($numwinP.find('span').eq(3).text());
+        console.log($numwinP.find('span').eq(4).text());
+        console.log($numwinP.find('span').eq(5).text());
+
+        // $numwinP.each(function (i, elem) {
+        //     const span = $(this).find('.ball1').text()
+        //     console.log('span',span);
+        // });
+        const $tbody = $("table.tbl_data tbody");
+        const $firstTr = $tbody.find('tr').eq(0);
+        const $secondTr = $tbody.find('tr').eq(1);
+        const $ThirdTr = $tbody.find('tr').eq(2);
+        const $fourthTr = $tbody.find('tr').eq(3);
+        const $fifthTr = $tbody.find('tr').eq(4);
+
+        console.log($fourthTr.find('td').eq(0).text());
+        console.log($fourthTr.find('td').eq(1).text());
+        console.log($fourthTr.find('td').eq(3).text());
+
+        // $tbodyTr.each(function (i, elem) {
+        //     const td = $(this).find('tr');
+        //     ulList[i]={
+        //         grade:anyToUtf8(td.eq(0).text()),
+        //         gradeTotSellamnt:anyToUtf8(td.eq(1).text()),
+        //         winamnt:anyToUtf8(td.eq(3).text())
+        //     }
+        //     // log('순위',anyToUtf8(td.eq(0).text()));
+        //     // log('등위별 총 당첨금액',anyToUtf8(td.eq(1).text()));
+        //     // log('당첨금액',anyToUtf8(td.eq(3).text()));
+        // });
+        // console.log(ulList);
     });
 
 
